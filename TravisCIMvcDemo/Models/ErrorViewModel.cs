d31d335353f85cf06d1e17978480f2f699975059
Cristian Eriomenco <cristian@rightindem.com>
2018-09-07 15:41:29 +0100

    Initial

---
+++ TravisCIMvcDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TravisCIMvcDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
